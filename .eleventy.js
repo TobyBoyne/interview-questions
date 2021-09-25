bf9c04eddcca3bc4088b4bbc8c83fd6af1496ec6
Toby Boyne <tobyboyne@gmail.com>
2021-09-25 22:31:06 +0100

    Added playlists

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,30 @@ module.exports = function(eleventyConfig) {
     // 11tydata.json files add tags instead of replace them
     eleventyConfig.setDataDeepMerge(true);
 
+    // Question as shortcode
+    eleventyConfig.addPairedShortcode("question", function(content) {
+        let qha = content.split('###')
+        return `<section class="question-part">
+            <div class="question">${qha[0]}</div>
+            <div class="hint">${qha[1]}</div>
+            <div class="answer">${qha[2]}</div>
+        </section>`});
+
+    
+    // Add playlists to collections
+    const playlists = require('./src/_data/playlists.json')
+    for (let playlistid in playlists) {
+        let playlist = playlists[playlistid]
+
+        eleventyConfig.addCollection(playlistid, function(collectionApi) {
+            return collectionApi.getAll().filter(function(a) {
+                return playlist.questions.includes(a.data.title)
+            }).sort(function(a, b) {
+                return playlist.questions.indexOf(a.data.title) - playlist.questions.indexOf(b.data.title)
+            });
+        });
+    };
+
     return {
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
